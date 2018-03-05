89201b09a2540be15665bec7649c0ecf5d02e38d
Jonathan Aaron <jon.r.aaron@gmail.com>
2018-03-04 17:21:32 -0700

    Change name of endpoint var to match the actual endpoint

--- src/api/deckOfCardsApi.js
+++ src/api/deckOfCardsApi.js
@@ -6,9 +6,9 @@ import {
 import { handleRequest } from './apiUtils'
 
 function getDeckOfCardsEpic(action$) {
+
   return handleRequest(action$, GET_DECK_OF_CARDS, getDeckOfCardsSuccess, getDeckOfCardsFail, () => {
-    const url = process.env.REACT_APP_DECK_OF_CARDS_API_ENDPOINT
-    console.log('url: ', process.env.REACT_APP_DECK_OF_CARDS_API_ENDPOINT)
+    const url = process.env.REACT_APP_DECKOFCARDS_DOT_COM_API_DECK
     return axios.get(`${url}new/shuffle/?deck_count=1`)
       .then((response) => {
         return response.data
@@ -18,7 +18,7 @@ function getDeckOfCardsEpic(action$) {
 
 function drawACardEpic(action$) {
   return handleRequest(action$, DRAW_A_CARD, drawACardSuccess, drawACardFail, ({payload: {deckId}}) => {
-    return axios.get(`${process.env.REACT_APP_DECK_OF_CARDS_API_ENDPOINT}${deckId}/draw/?count=2`)
+    return axios.get(`${process.env.REACT_APP_DECKOFCARDS_DOT_COM_API_DECK}${deckId}/draw/?count=2`)
       .then((response) => {
         return response.data
       })
